413fcc0bb25d17d91320dd504d290f089e199a42
shawn <shawnantonucci2018@gmail.com>
2019-04-17 00:09:48 -0700

    create new user from phone number

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { Input, Button } from "react-native-elements";
+import axios from "axios";
+
+const ROOT_URL =
+  "https://us-central1-one-time-password-86a43.cloudfunctions.net";
 
 class SignUpForm extends Component {
+  state = {
+    phone: ""
+  };
+
+  handleSubmit = () => {
+    axios
+      .post(`${ROOT_URL}/createUser`, {
+        phone: this.state.phone
+      })
+      .then(() => {
+        axios.post(`${ROOT_URL}/requestOneTimePassword`, {
+          phone: this.state.phone
+        });
+      });
+  };
+
   render() {
     return (
       <View>
         <View style={{ marginBottom: 10 }}>
-          <Input label="Please Enter your phone number" />
+          <Input
+            label="Please Enter your phone number"
+            value={this.state.phone}
+            onChangeText={phone => this.setState({ phone })}
+          />
         </View>
-        <Button title="Submit" />
+        <Button title="Submit" onPress={this.handleSubmit} />
       </View>
     );
   }
